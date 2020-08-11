5c279afa424c375a34c920d0b9e17e742a0701d4
John Kim <johnk9000@gmail.com>
2020-08-11 14:31:10 -0500

    Last additions to get deployment to work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-const mongocxn = process.env.MONGOLAB_URI || "mongodb://localhost/budget"
+const mongocxn = process.env.MONGODB_URI || "mongodb://localhost/budget"
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(mongocxn, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
